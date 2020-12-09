c17aab05569fa979b0d54f18a614dc8c57c25089
Gladwinnnn <gladwin1503@hotmail.com>
2020-12-09 20:29:49 +0800

    continue with tutorial

--- src/App.js
+++ src/App.js
@@ -1,16 +1,68 @@
 import React from "react";
 
 import TodoItem from "./TodoItem";
+import todosData from "./todosData";
 
-function App() {
-    return (
-        <div className="todo-list">
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
-        </div>
-    )
+// function App() {
+//     const todoComponent = todosData.map(list => 
+//         <TodoItem 
+//             key={list.id}
+//             todoItem={list}
+//         />
+//     );
+
+//     return (
+//         <div className="todo-list">
+//             {todoComponent}
+//         </div>
+//     )
+// }
+
+// class App extends React.Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             status: "Completed"
+//         };
+//     }
+
+//     render() {
+//         const todoComponent = todosData.map(list => 
+//             <TodoItem 
+//                 key={list.id}
+//                 todoItem={list}
+//             />
+//         );
+    
+//         return (
+//             <div className="todo-list">
+//                 {todoComponent}
+//             </div>
+//         )
+//     }
+// }
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            isLoggedIn: true
+        }
+    }
+
+    render() {
+        let display;
+        if (this.state.isLoggedIn == true){
+            display = "in";
+        } else{
+            display = "out";
+        }
+        return (
+            <div>
+                <h1>You are currently logged {display}</h1>
+            </div>
+        )
+    }
 }
 
 export default App
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-function TodoItem() {
+function TodoItem(props) {
     return (
         <div className="todo-item">
-            <input type="checkbox"></input>
-            <p> React JS Tutorial</p>
+            <input type="checkbox" checked={props.todoItem.completed}></input>
+            <p>{props.todoItem.text}</p>
         </div>
     )
 }
